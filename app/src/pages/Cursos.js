19b33da631e08638d920f27b8f31961625c4408f
n-qber <nmkbertozzo@gmail.com>
2024-01-22 14:16:56 -0300

    fix: cursos route was not working

--- app/src/pages/Cursos.js
+++ app/src/pages/Cursos.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '../App.css';
 
-import chapeu from "../img/chapeu.svg";
 
 const Cursos = () => {
     
 
 
 }
+export default Cursos;
