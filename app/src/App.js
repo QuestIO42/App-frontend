afd9f8d42b6e248f589f866d926fe50948cfd299
n-qber <nmkbertozzo@gmail.com>
2024-01-24 11:42:59 -0300

    register frontend done

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Cursos from './pages/Cursos';
+import Register from './pages/Register';
 
 function App() {
   return (
       <Router>
           <Routes>
               <Route path="/" element={<Login/>} />
+              <Route path="/cadastrar" element={<Register />} />
               <Route path="/cursos" element={<Cursos />} />
           </Routes>
       </Router>
