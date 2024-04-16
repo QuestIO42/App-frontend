bad8506a58a4405dad49d7d11a76803ed5e2db3b
n-qber <nmkbertozzo@gmail.com>
2024-04-16 09:08:57 -0300

    subjects in cursos page

---
+++ app/src/components/SubjectSection.jsx
@@ -0,0 +1,21 @@
+import SubjectTitle from './SubjectTitle';
+
+import LightBulb from './light-bulb.svg';
+import './SubjectSection.css';
+
+
+export default function SubjectSection(props) {
+    return (
+        <div className="section">
+            <div className="section-title">
+                <img src={LightBulb} alt="" />
+                <h4>Principais portas lógicas</h4>
+            </div>
+
+            <div className="subjects">
+                <SubjectTitle />
+                <SubjectTitle />
+            </div>
+        </div>
+    )
+}
