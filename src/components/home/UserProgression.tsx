e085733b334323172c540a5553fb55ef0ad93e62
sgmaykon <sgmaykon@estudante.ufscar.br>
2024-07-28 15:24:58 -0300

    Add: circle progress; Fix: ProgressBar

--- src/components/home/UserProgression.tsx
+++ src/components/home/UserProgression.tsx
@@ -4,16 +4,17 @@ import ProgressCircle from './ProgressXpCircle'
 interface UserProgressionProps {
   username?: string
 }
+/* Os valores serão dados obtidos do usuario pela chama da api */
 
 export default function UserProgression({ username }: UserProgressionProps) {
   return (
-    <div className="grid h-40 w-[558px] grid-cols-2 rounded-lg border-4 border-preto-default bg-branco p-4 shadow-default-preto">
+    <div className="grid h-40 w-[558px] grid-cols-2 border-4 border-preto-default bg-branco p-4 shadow-default-preto">
       <div className="flex flex-col">
-        <h2 className="text-xl font-bold">ola {username} !</h2>
-        <ProgressXpBar value={100}></ProgressXpBar>
+        <h2 className="text-xl center font-bold">olá {username} !</h2>
+        <ProgressXpBar value={75}></ProgressXpBar>
       </div>
-      <div className="ml-auto flex items-center">
-        <ProgressCircle percentage={50}></ProgressCircle>
+      <div className="ml-auto flex-col items-center">
+        <ProgressCircle level={1} progress={75}></ProgressCircle>
       </div>
     </div>
   )
