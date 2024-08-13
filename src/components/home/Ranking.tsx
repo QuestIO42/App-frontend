f90734f7d1f561045fec6e7b05324daf0ead606e
sgmaykon <sgmaykon@estudante.ufscar.br>
2024-08-12 22:36:38 -0300

    fix Ranking Interface

--- src/components/home/Ranking.tsx
+++ src/components/home/Ranking.tsx
@@ -1,7 +1,10 @@
 import RewardIcon from '../svgComponents/icons/RewardIcon'
 
+interface RankingProps {
+  children: React.ReactNode
+}
 
-export default function Ranking(Children:any) {
+export default function Ranking(Children: RankingProps) {
   return (
     <div className="flex flex-col gap-5">
       <div className="ml-20 shadow-default-roxo-500 flex min-h-[70px] max-w-[73%] items-center justify-center bg-roxo-300">
