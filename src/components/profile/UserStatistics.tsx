aafab82e30ed610edcc2a782b662388cbafcb59f
sgmaykon <sgmaykon@estudante.ufscar.br>
2024-08-30 19:58:45 -0300

    fix: progressPage margin

--- src/components/profile/UserStatistics.tsx
+++ src/components/profile/UserStatistics.tsx
@@ -11,7 +11,7 @@ export default function UserStatistics() {
     <aside className="flex h-auto flex-col items-center justify-center border-4 border-preto-default bg-branco p-4 shadow-default-preto md:min-w-[25rem]">
       <h2 className="center text-2xl font-bold">estatísticas</h2>
 
-      <div className="flex  w-2/3 flex-col justify-center">
+      <div className="flex  w-2/3 mt-4 flex-col justify-center">
         <ProgressXpCircle level={1} progress={70}></ProgressXpCircle>
         <ProgressXpBar value={75}></ProgressXpBar>
         <UserRank></UserRank>
