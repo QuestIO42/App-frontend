47118a56acb0ed5d651a53969051080c34909c08
sgmaykon <sgmaykon@estudante.ufscar.br>
2024-09-24 09:46:15 -0300

    fix: xp display

--- src/components/home/UserProgression.tsx
+++ src/components/home/UserProgression.tsx
@@ -10,7 +10,7 @@ export default function UserProgression({ username }: UserProgressionProps) {
     <div className="grid h-40 w-[558px] grid-cols-2 border-4 border-preto-default bg-branco p-4 shadow-default-preto">
       <div className="flex flex-col">
         <h2 className="center text-xl font-bold">olá, {username} !</h2>
-        <ProgressXpBar value={75}></ProgressXpBar>
+        <ProgressXpBar text="xp" value={75}></ProgressXpBar>
       </div>
       <div className="ml-auto flex  items-center justify-center">
         <ProgressXpCircle level={1} progress={75}></ProgressXpCircle>
