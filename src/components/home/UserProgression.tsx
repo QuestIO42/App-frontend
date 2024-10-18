e042e71ccab6b7dd34793b9062ef5a51d8519a1e
Nicole Ramos <147672912+Ninn-up@users.noreply.github.com>
2024-10-18 13:28:05 -0300

    Code refactoring 1.2
    Adjusts from the first review

--- src/components/home/UserProgression.tsx
+++ src/components/home/UserProgression.tsx
@@ -5,10 +5,22 @@ interface UserProgressionProps {
   username?: string
 }
 
+/*function GetGap({ username }: UserProgressionProps): string {
+  if (!username) return '3';
+  
+  const length = username.length;
+
+  if (length < 10) return '3';
+  if (length < 20) return '4';
+
+  return '4';
+};*/
+
 export default function UserProgression({ username }: UserProgressionProps) {
+  username = 'Samuel Medeiros'
   return (
     <div className="grid min-h-[193px] min-w-[558px] grid-cols-2 border-4 border-preto-default bg-branco pt-4 pb-3 pl-12 pr-0 shadow-default-preto">
-      <div className="flex flex-col gap-3">   {/* Talvez seja necessário aumentar/diminuir o gap caso o nome do usuário não caiba no espaço atual disponível */}
+      <div className="flex flex-col">   {/* Talvez seja necessário aumentar/diminuir o gap caso o nome do usuário não caiba no espaço atual disponível */}
         <h2 className="center text-4xl font-bold text-cinza pt-2">olá, {username}!</h2>
         <ProgressXpBar text="xp" value={75}></ProgressXpBar>
       </div>
