4ff44a57b7cb8eca7732c1340deaf68e919ce5af
sgmaykon <sgmaykon@estudante.ufscar.br>
2024-10-26 16:45:27 -0300

    fix: positioning

--- src/components/utility/ModalSquareForm.tsx
+++ src/components/utility/ModalSquareForm.tsx
@@ -8,21 +8,10 @@ interface ModalSquareFormProps {
 
 export default function ModalSquareForm({ children, courseName, courseTeacher }: ModalSquareFormProps) {
   return (
-    <div className="flex flex-col h-fit w-fit">
-
-      <div className="relative flex h-fit w-fit items-center justify-center">
-        <div className="absolute bottom-0 left-0 h-[104%] w-[105%] border-4 border-cinza"></div>
-        <div className="absolute right-0 top-0 h-[104%] w-[105%] border-4 border-cinza"></div>
-        {children}
-
-      </div>
-      
-        <p className="mt-3 text-xl text-left font-bold underline text-cinza"> 
-        { courseName }
-        </p>
-        <p className="mt-1 text-left font-bold text-cinzaClaro">
-          { courseTeacher }
-        </p>
+    <div className="relative flex h-fit w-fit p-1 items-center justify-center">
+      <div className="absolute bottom-[2px] left-[2px] h-[104%] w-[105%] border-4 border-cinza"></div>
+      <div className="absolute right-[2px] top-[2px] h-[104%] w-[105%] border-4 border-cinza"></div>
+      {children}
     </div>
   )
 }
