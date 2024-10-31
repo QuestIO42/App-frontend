9de06c1172f74972dcf1d8b9e642076b5ee94571
sgmaykon <sgmaykon@estudante.ufscar.br>
2024-10-31 12:27:47 -0300

    Refactor: Padrão de campos adotados

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -8,7 +8,7 @@ export default function UserProfile() {
   return (
     <section className="relative flex flex-col items-center justify-center gap-4 lg:max-w-[27rem]">
       <Button
-        className="absolute right-0 top-0"
+        className="absolute right-0 bg-white top-0"
         text="editar"
         size="small"
         variant="secondary"
@@ -16,7 +16,7 @@ export default function UserProfile() {
       <ProfileIcon className="mt-10 h-[160px] w-[160px] text-verde-300"></ProfileIcon>
       <h2 className="text-5xl font-bold text-cinza">{user?.username}</h2>
       <StudentInformation />
-      <Button className="text-cinza" text={'minhas turmas'}></Button>
+      <Button className="text-cinza bg-white" text={'minhas turmas'}></Button>
     </section>
   )
 }
