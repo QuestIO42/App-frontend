0203c9693acf5f10eac5eb94e613a1f9a3b7ca69
Enzo Murayama <enzomurayama@gmail.com>
2025-06-02 08:33:07 -0300

    chore: remove unused buttons on profile page

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -7,16 +7,16 @@ export default function UserProfile() {
   const { user } = useAuth()
   return (
     <section className="relative flex flex-col items-center justify-center gap-4 lg:max-w-[27rem]">
-      <Button
+      {/* <Button
         className="absolute right-0 top-0 bg-white"
         text="editar"
         size="small"
         variant="secondary"
-      ></Button>
+      ></Button> */}
       <ProfileIcon className="mt-10 h-[160px] w-[160px] text-verde-300"></ProfileIcon>
       <h2 className="text-5xl font-bold text-cinza">{user?.username}</h2>
       <StudentInformation />
-      <Button className="bg-white text-cinza" text={'minhas turmas'}></Button>
+      {/* <Button className="bg-white text-cinza" text={'minhas turmas'}></Button> */}
     </section>
   )
 }
