632f472898e7529260c77850ea49e41847da3fa8
andre.rettondini <andre.rettondini@catijr.com.br>
2025-06-05 18:53:42 -0300

    fixed errors size on login/register/forgotpassword

--- src/components/form/ErrorMessage.tsx
+++ src/components/form/ErrorMessage.tsx
@@ -4,6 +4,6 @@ interface ErrorMessageProps {
 
 export default function ErrorMessage({ error }: ErrorMessageProps) {
   return (
-    <div className="mr-auto text-[0.5rem] text-red-500 md:text-xs">{error}</div>
+    <div className="mr-auto text-[0.9rem] text-red-500 md:text-xs">{error}</div>
   )
 }
