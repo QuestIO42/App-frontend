d35b7ae3936b9fff2f97f173721c9aa28296dce9
Enzo Murayama <enzomurayama@gmail.com>
2025-06-29 19:27:31 -0300

    chore: remove comments on footer page

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,29 +7,6 @@ export default function Footer() {
 
   return (
     <footer className="bg-roxo-900 bottom-0 flex min-h-[150px] w-screen flex-col items-start sm:items-center justify-center px-6 py-8">
-      {/* <div className="flex w-full items-center justify-around">
-        <div className="flex flex-1 flex-col items-center justify-start">
-          <p className="text-2xl font-bold text-amarelo">Contato</p>
-          <div className="flex items-center justify-center gap-2">
-            <EmailIcon></EmailIcon>
-            <p>questio@ufscar.br</p>
-          </div>
-        </div>
-
-        <p className="flex-1 text-center text-2xl font-bold text-amarelo">
-          Quem somos
-        </p>
-        <p className="flex-1 text-center text-2xl font-bold text-amarelo">
-          Minha conta
-        </p>
-      </div>
-
-      <div className="flex flex-col items-center justify-center">
-        <p className="text-center text-xs font-semibold">
-          - 2024 | <a href="https://questio42.github.io/">questio42.github.io</a> -
-        </p>
-        <p className="text-center text-[0.63rem]">by QuestI0 Lab</p>
-      </div> */}
       <div className="flex flex-col sm:flex-row py-4 gap-6 sm:gap-20">
         <div className="">
           <h3 className="font-bold text-2xl text-verde-300">QuestIO</h3>
