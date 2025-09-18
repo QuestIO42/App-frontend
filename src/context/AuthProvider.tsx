03e5dd056daf24295bedd6baa11164c558816fed
Enzo Murayama <enzomurayama@gmail.com>
2025-09-18 18:24:03 -0300

    fix: auth provider

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -2,13 +2,13 @@ import { createContext, useState, useLayoutEffect, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { api } from '@/services/api/api'
 import { AxiosError } from 'axios'
-import Cookies from 'js-cookie'
 import { SignInCredentials } from '@/interfaces/SignInCredentials'
 import { User } from '@/interfaces/User'
 import { mockUser } from '@/utils/mockUser'
 import { jwtDecode } from 'jwt-decode'
 import { signInUser, logout } from '@/services/api/auth'
 import { getUser } from '@/services/api/user'
+import Cookies from 'js-cookie'
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>
@@ -23,25 +23,21 @@ type AuthProviderProps = {
   children: React.ReactNode
 }
 
-interface FailedRequest {
-  onSuccess: (accessToken: string) => void
-  onFailure: (error: AxiosError) => void
-}
+export const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
 let isRefreshing = false
-let failedRequestQueue: FailedRequest[] = []
-
-export const AuthContext = createContext<AuthContextData>({} as AuthContextData)
+let failedRequestQueue: {
+  onSuccess: (accessToken: string) => void
+  onFailure: (error: AxiosError) => void
+}[] = []
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User | null>(null)
   const [isLoading, setIsLoading] = useState(true)
-
-  const [accessToken, setToken] = useState<string | null>(() => {
-    return Cookies.get('accessToken') || null
-  })
-
   const navigate = useNavigate()
+
+  const accessToken = Cookies.get('accessToken')
+
   const isAuthenticated = import.meta.env.VITE_APP_ENV == 'development' ? true : !!accessToken
 
   useLayoutEffect(() => {
@@ -68,23 +64,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   fetchUser()
  }, [accessToken])
 
-  useLayoutEffect(() => {
-    if (import.meta.env.VITE_APP_ENV !== 'development') {
-      const authInterceptor = api.interceptors.request.use(
-        (config) => {
-          if (accessToken) {
-            config.headers.Authorization = `Bearer ${accessToken}`
-          }
-          return config
-        },
-        (error) => Promise.reject(error)
-      )
-      return () => {
-        api.interceptors.request.eject(authInterceptor)
-      }
-    }
-  }, [accessToken])
-
+  // Refresh automático no 401
   useEffect(() => {
     if (import.meta.env.VITE_APP_ENV !== 'development') {
       const refreshInterceptor = api.interceptors.response.use(
@@ -93,13 +73,6 @@ export function AuthProvider({ children }: AuthProviderProps) {
           const originalRequest = error.config;
 
           if (!error.response || error.response.status !== 401) {
-            return Promise.reject(error);
-          }
-
-          const data = error.response.data;
-
-          if (data.code !== 'token_not_valid' && data.message !== 'Token expired.') {
-            signOut();
             return Promise.reject(error);
           }
 
@@ -117,13 +90,10 @@ export function AuthProvider({ children }: AuthProviderProps) {
               const response = await api.post('/auth/token/refresh', { refresh: refreshToken });
               const newAccessToken = response.data.access;
 
-              setToken(newAccessToken);
               Cookies.set('accessToken', newAccessToken, {
                 sameSite: 'Lax',
                 secure: import.meta.env.PROD,
               });
-
-              api.defaults.headers['Authorization'] = `Bearer ${newAccessToken}`;
 
               // Resolve todas as requisições que estavam na fila
               failedRequestQueue.forEach(req => req.onSuccess(newAccessToken));
@@ -156,22 +126,19 @@ export function AuthProvider({ children }: AuthProviderProps) {
         api.interceptors.response.eject(refreshInterceptor);
       };
     }
-  }, [accessToken]);
+  }, []);
 
   async function signIn({ login, password }: SignInCredentials) {
     try {
       if (import.meta.env.VITE_APP_ENV === 'development') {
         // Em desenvolvimento, simula um usuário autenticado
-        setToken('fake-token')
         setUser(mockUser)
-        Cookies.set('token', 'fake-token')
+        Cookies.set('accessToken', 'fake-token')
         navigate('/home')
       } else {
         const response = await signInUser({ login, password })
-
         const accessToken = response.access
         const refreshToken = response.refresh
-        setToken(accessToken)
 
         Cookies.set('accessToken', accessToken, {
           sameSite: 'Lax',
@@ -184,10 +151,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         })
 
         const { sub } = jwtDecode<{ sub: string }>(accessToken)
-
         await fetchPerson(sub)
         navigate('/home')
-        console.log('Login realizado com sucesso')
       }
     } catch (error) {
       console.error('Erro no login:', error)
@@ -204,22 +169,12 @@ export function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
-  async function signOut() {
+  function signOut() {
     try {
-      if (import.meta.env.VITE_APP_ENV === 'development') {
-        // Em desenvolvimento, limpa a simulação de autenticação
-        setToken(null)
-        setUser(null)
-        Cookies.remove('accessToken')
-        Cookies.remove('refreshToken')
-        navigate('/')
-      } else {
-        if (accessToken) {
-          await logout()
-        }
+      if (import.meta.env.VITE_APP_ENV !== 'development') {
+        logout()
       }
     } finally {
-      setToken(null)
       setUser(null)
       Cookies.remove('accessToken')
       Cookies.remove('refreshToken')
@@ -240,7 +195,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   return (
     <AuthContext.Provider
-      value={{ signIn, signOut, configPass, isAuthenticated, user, isLoading  }}
+      value={{ signIn, signOut, configPass, isAuthenticated, user, isLoading }}
     >
       {children}
     </AuthContext.Provider>
