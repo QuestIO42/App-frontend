1e26d5f9a0325717e3987fe076c44a030b0bc102
Enzo Murayama <enzomurayama@gmail.com>
2025-10-10 11:07:18 -0300

    refactor: add new links to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,12 @@
-import { useAuth } from '@/hooks/useAuth'
 import LinkedinIcon from '../svgComponents/icons/linkedin'
 import EmailIcon from '../svgComponents/icons/EmailIcon'
 
 export default function Footer() {
-  const { user } = useAuth()
-
   return (
     <footer className="bg-roxo-900 bottom-0 flex min-h-[150px] w-screen flex-col items-start sm:items-center justify-center px-6 py-8">
       <div className="flex flex-col sm:flex-row py-4 gap-6 sm:gap-20">
         <div className="">
-          <h3 className="font-bold text-2xl text-verde-300">QuestIO</h3>
+          <a href="https://www.vlab.dc.ufscar.br" className="font-bold text-2xl text-verde-300 transition-color duration-300 hover:text-[#2eb875]">QuestIO</a>
         </div>
 
         <div className="flex flex-row flex-wrap gap-4">
@@ -20,15 +17,27 @@ export default function Footer() {
           </a>
 
           <a
-            href={`/profile/${user?.id}`}
+            href={`https://www.vlab.dc.ufscar.br/tutoriais`}
             className="flex items-center text-roxo-500 text-sm font-semibold py-2 px-4 border border-roxo-500 rounded-full transition delay-10 duration-200 ease-in-out hover:border-[#9386d9] hover:text-[#9386d9]">
-            Perfil
+            Tutoriais
           </a>
 
           <a
-            href={`https://www.vlab.dc.ufscar.br/`}
+            href={`https://legacy.vlab.dc.ufscar.br/camera.php`}
             className="flex items-center text-roxo-500 text-sm font-semibold py-2 px-4 border border-roxo-500 rounded-full transition delay-10 duration-200 ease-in-out hover:border-[#9386d9] hover:text-[#9386d9]">
-            Sobre
+            Vlab
+          </a>
+
+          <a
+            href={`https://legacy.vlab.dc.ufscar.br/agenda`}
+            className="flex items-center text-roxo-500 text-sm font-semibold py-2 px-4 border border-roxo-500 rounded-full transition delay-10 duration-200 ease-in-out hover:border-[#9386d9] hover:text-[#9386d9]">
+            Agenda
+          </a>
+
+          <a
+            href={`https://www.vlab.dc.ufscar.br/equipe.html`}
+            className="flex items-center text-roxo-500 text-sm font-semibold py-2 px-4 border border-roxo-500 rounded-full transition delay-10 duration-200 ease-in-out hover:border-[#9386d9] hover:text-[#9386d9]">
+            Equipe
           </a>
         </div>
 
