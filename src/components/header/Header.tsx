27e30e15fb9140417521ea2d6647dc05eb6d9536
Enzo Murayama <enzomurayama@gmail.com>
2025-10-17 12:06:08 -0300

    style: update header size for mobile devices

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,16 +8,16 @@ export default function Header() {
   const { user } = useAuth()
 
   return (
-    <header className="col-span-full flex h-[100px] w-full items-center justify-between bg-roxo-900 px-8 md:px-16">
+    <header className="col-span-full flex py-3 md:h-[100px] w-full items-center justify-between bg-roxo-900 px-8 md:px-16">
       <div className="flex items-center">
-        <Logo link={'/home'} className="w-12 text-verde-300 transition-all duration-300 ease-in-out hover:text-[#2eb875] active:text-[#2eb875]" />
+        <Logo link={'/home'} className="w-9 md:w-12 text-verde-300 transition-all duration-300 ease-in-out hover:text-[#2eb875] active:text-[#2eb875]" />
       </div>
 
       <div className="flex flex-1 items-center justify-end gap-2">
-        <NotificationIcon className="w-11 aspect-square" />
+        <NotificationIcon className="w-9 md:w-11 aspect-square" />
 
         <Link to={`/profile/${user?.id}`} className="group">
-          <ProfileIcon className="w-11 aspect-square" />
+          <ProfileIcon className="w-9 md:w-11 aspect-square" />
         </Link>
       </div>
     </header>
